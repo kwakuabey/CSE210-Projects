239c7b3df5d7eb05aea98daceea6046ff0bcad5d
kwakuabey <babbey@byupathway.edu>
2026-06-16 20:22:24 +0000

    added tracking exercise

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2026, 06, 12), 30, 4.8),
+            new Cycling(new DateTime(2026, 06, 15), 45, 25.0),
+            new Swimming(new DateTime(2026, 06, 16), 40, 30)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
